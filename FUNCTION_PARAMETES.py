575440b1663e62f300915ef577cdd913ef363d66
venkat220604 <venkatadheerajgottapu@gmail.com>
2025-07-08 21:07:16 +0530

    Add files via upload

---
+++ FUNCTION_PARAMETES.py
@@ -0,0 +1,5 @@
+def intro(name="Dheeraj", branch="ECE"):
+    print(f"Name: {name}, Branch: {branch}")
+
+intro()
+intro("Ashritha", "CSE")
---
+++ LOOPS.py
@@ -0,0 +1,12 @@
+#for loop
+for i in range(1,6):
+    print(i)
+#while loop
+while i<= 5:
+    print(i)
+    i+=1
+#enumerate 
+#used for index  + value
+fruits=["apple","banana","mango"]
+for index,value in enumerate(fruits):
+    print(f"{index}->{value}")
